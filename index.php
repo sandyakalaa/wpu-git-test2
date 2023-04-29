2ccb5805ecc566c043b3d9aebb330add2d1f65fe
sandyakalaa <aylasaraxyz@gmail.com>
2023-04-29 10:39:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex eligendi porro quasi in et aperiam magnam, assumenda voluptates nostrum amet aut quas repudiandae recusandae molestias laborum hic dolorum nobis magni.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
